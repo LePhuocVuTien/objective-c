59a71c1828968f8f19736e0212ae1b1474d95d01
VuTien <tienle.clc11t@gmail.com>
2019-06-17 09:28:44 +0700

    -StartProject

---
+++ objective-C/objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objective-C
+//
+//  Created by iMacbook on 6/17/19.
+//  Copyright © 2019 IOTLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objective-C/objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objective-C
+//
+//  Created by iMacbook on 6/17/19.
+//  Copyright © 2019 IOTLink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
